2d3605e1f6340004430221e1c6c96ecd56773988
Ezequiel <ezequieldannus@gmail.com>
2024-08-22 07:58:49 -0300

    mini banco

--- clima_tempo/views/default.py
+++ clima_tempo/views/default.py
@@ -1,86 +1,73 @@
 import requests
 import sqlite3
 from pyramid.view import view_config
+from googletrans import Translator
 
-# Conectar ao banco de dados SQLite
-conn = sqlite3.connect('weather_data.db')
-c = conn.cursor()
+def get_db_connection():
+    conn = sqlite3.connect('weather_data.db')
+    conn.row_factory = sqlite3.Row  
+    return conn
 
-# Criar as tabelas para armazenar dados do clima e contagem de pesquisas
-c.execute('''
-CREATE TABLE IF NOT EXISTS clima (
-    id INTEGER PRIMARY KEY,
-    cidade TEXT NOT NULL,
-    temperatura REAL,
-    descricao TEXT,
-    umidade INTEGER,
-    pressao INTEGER,
-    velocidade_vento REAL,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-''')
+def incrementar_contagem_cidade(cidade):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT contagem FROM contagem_pesquisa WHERE cidade = ?", (cidade,))
+        result = cursor.fetchone()
+        if result:
+            nova_contagem = result['contagem'] + 1
+            cursor.execute("UPDATE contagem_pesquisa SET contagem = ? WHERE cidade = ?", (nova_contagem, cidade))
+        else:
+            cursor.execute("INSERT INTO contagem_pesquisa (cidade, contagem) VALUES (?, 1)", (cidade,))
+        conn.commit()
 
-c.execute('''
-CREATE TABLE IF NOT EXISTS contagem_pesquisa (
-    cidade TEXT PRIMARY KEY,
-    contagem INTEGER DEFAULT 0
-)
-''')
-conn.commit()
 
-def incrementar_contagem_cidade(cursor, cidade):
-    cursor.execute("SELECT contagem FROM contagem_pesquisa WHERE cidade = ?", (cidade,))
-    result = cursor.fetchone()
-    if result:
-        nova_contagem = result[0] + 1
-        cursor.execute("UPDATE contagem_pesquisa SET contagem = ? WHERE cidade = ?", (nova_contagem, cidade))
-    else:
-        cursor.execute("INSERT INTO contagem_pesquisa (cidade, contagem) VALUES (?, 1)", (cidade,))
-    cursor.connection.commit()
+def translate_text_googletrans(text, target_lang='pt'):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_lang)
+    return translation.text
+
+
+def get_contagem_cidades():
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT cidade, contagem FROM contagem_pesquisa ORDER BY contagem DESC")
+        return cursor.fetchall()
 
 @view_config(route_name="home", renderer='templates/home.jinja2')
 def home(request):
-    return {}
+    # Obter contagem de pesquisas para cada cidade
+    contagens = get_contagem_cidades()
+    return {'contagens': contagens}
 
 @view_config(route_name='weather', renderer='templates/home.jinja2')
 def weather(request):
-    city = request.params.get('city')
+    city = request.params.get('city', 'New York')
     api_key = 'd8d31293bee740761c9ba933823c09ea'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+
 
     try:
         response = requests.get(url)
         data = response.json()
+        descricao = data['weather'][0]['description']
+        descricao_traduzida = translate_text_googletrans(descricao)
+        
+        velocidade_vento = data['wind']['speed']*3.6
+
         weather_data = {
             'temperature': data['main']['temp'],
-            'description': data['weather'][0]['description'],
+            'description': descricao_traduzida,
             'humidity': data['main']['humidity'],
             'pressure': data['main']['pressure'],
-            'wind_speed': data['wind']['speed'],
+            'wind_speed': velocidade_vento,
         }
         
-        # Inserir dados do clima no banco de dados
-        c.execute('''
-        INSERT INTO clima (cidade, temperatura, descricao, umidade, pressao, velocidade_vento)
-        VALUES (?, ?, ?, ?, ?, ?)
-        ''', (city, weather_data['temperature'], weather_data['description'],
-              weather_data['humidity'], weather_data['pressure'], weather_data['wind_speed']))
+        # Atualizar contagem de pesquisa para a cidade
+        incrementar_contagem_cidade(city)
         
-        # Incrementar a contagem de pesquisas para a cidade
-        incrementar_contagem_cidade(c, city)
+        contagens = get_contagem_cidades()
         
-        conn.commit()
-        
-        return {'weather_data': weather_data}
-    except Exception as e:
-        # Fechar conexão no banco de dados em caso de erro
-        conn.close()
-        return {'error': "Cidade não encontrada, ou digitada incorretamente"}
+        return {'weather_data': weather_data, 'contagens':contagens }
+    except requests.exceptions.RequestException as e:
+        return {'error': "Erro ao acessar o serviço de clima."}
 
-# Função para consultar a quantidade de pesquisas por cidade
-def consultar_contagem_cidades():
-    c.execute("SELECT cidade, contagem FROM contagem_pesquisa")
-    return c.fetchall()
-
-# Fechar conexão no banco de dados quando não estiver mais sendo usada
-conn.close()
